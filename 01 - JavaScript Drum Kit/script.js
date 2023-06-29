d6ea11dee45c37a18652e1f2caf8616f643940ad
imvot <110433315+imvot@users.noreply.github.com>
2023-06-29 11:23:38 -0400

    Add sounds for keys
    Adds dict for DOM audio objects
    Adds function to play and stop sounds

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -13,13 +13,18 @@ const keyCode = [
 const keys = {}
 document.querySelectorAll(".key").forEach((key, i) => keys[keyCode[i]] = key )
 
+const sounds = {}
+document.querySelectorAll("audio").forEach((sound, i) => sounds[keyCode[i]] = sound )
+
 window.addEventListener("keydown", event => {
     console.log(event.code)
     keyPress(keys[event.code])
+    playSound(sounds[event.code])
 })
 
 window.addEventListener("keyup", event => {
     console.log(event.code)
+    pauseSound(sounds[event.code])
     keyRelease(keys[event.code])
 })
 
@@ -29,4 +34,13 @@ function keyPress(keyObject) {
 
 function keyRelease(keyObject) {
     keyObject.classList.remove("playing")
+}
+
+function playSound(soundObject) {
+    soundObject.play()
+}
+
+function pauseSound(soundObject) {
+    soundObject.currentTime = 0
+    soundObject.pause()
 }
